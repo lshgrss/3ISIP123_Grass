af82884f1cb796276c70b49f0a956f423a2fd9e2
lshgrss <noxpox555@gmail.com>
2025-09-18 11:20:20 +0300

    Добавлена функция пузырьковой сортировки

--- 3ISIP123_Grass/main.cpp
+++ 3ISIP123_Grass/main.cpp
@@ -13,6 +13,17 @@ void clearInputBuffer() {
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
 
+void bubbleSort(vector<Expense>& expenses) {
+	int n = expenses.size();
+	for (int i = 0; i < n - 1; ++i) {
+		for (int j = 0; j < n - i - 1; ++j) {
+			if (expenses[j].amount > expenses[j + 1].amount) {
+				swap(expenses[j], expenses[j + 1]);
+			}
+		}
+	}
+}
+
 int main() {
 	int numOperations;
 
